bcad2612a1f069683ad6048b84aedc57d513fa75
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-02-12 13:56:54 +0530

    video player completed

--- lib/Screens/CameraScreen.dart
+++ lib/Screens/CameraScreen.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:whatsapp/Screens/CameraView.dart';
+import 'package:whatsapp/Screens/VideoView.dart';
 
 List<CameraDescription>? cameras;
 
@@ -15,6 +18,10 @@ class CameraScreen extends StatefulWidget {
 class _CameraScreenState extends State<CameraScreen> {
   CameraController? _cameraController;
   Future<void>? cameraValue;
+  bool isRecording = false;
+  bool flash = false;
+  bool isCamera = true;
+  double transform = 0;
   @override
   void initState() {
     // TODO: implement initState
@@ -66,32 +73,76 @@ class _CameraScreenState extends State<CameraScreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            flash = !flash;
+                          });
+                          flash
+                              ? _cameraController?.setFlashMode(FlashMode.torch)
+                              : _cameraController?.setFlashMode(FlashMode.off);
+                        },
                         icon: Icon(
-                          Icons.flash_off,
+                          flash ? Icons.flash_on : Icons.flash_off,
                           color: Colors.white,
                           size: 28,
                         ),
                       ),
-                      InkWell(
-                        onTap: () {},
-                        child: IconButton(
-                          onPressed: () {
+                      GestureDetector(
+                        onLongPress: () async {
+                          await _cameraController?.startVideoRecording();
+                          setState(() {
+                            isRecording = true;
+                          });
+                        },
+                        onLongPressUp: () async {
+                          final video =
+                              await _cameraController?.stopVideoRecording();
+                          setState(() {
+                            isRecording = false;
+                          });
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (builder) =>
+                                    VideoView(path: video!.path),
+                              ));
+                        },
+                        onTap: () {
+                          if (!isRecording) {
                             takePhoto(context);
-                          },
-                          icon: Icon(
-                            Icons.panorama_fish_eye,
-                            color: Colors.white,
-                            size: 70,
-                          ),
-                        ),
+                          }
+                        },
+                        child: isRecording
+                            ? Icon(
+                                Icons.radio_button_on,
+                                color: Colors.red,
+                                size: 80,
+                              )
+                            : Icon(
+                                Icons.panorama_fish_eye,
+                                color: Colors.white,
+                                size: 70,
+                              ),
                       ),
                       IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.flip_camera_ios,
-                          color: Colors.white,
-                          size: 28,
+                        onPressed: () async {
+                          setState(() {
+                            isCamera = !isCamera;
+                            transform = transform + pi;
+                          });
+                          int cameraPos = isCamera ? 0 : 1;
+
+                          _cameraController = CameraController(
+                              cameras![cameraPos], ResolutionPreset.high);
+                          cameraValue = _cameraController?.initialize();
+                        },
+                        icon: Transform.rotate(
+                          angle: transform,
+                          child: Icon(
+                            Icons.flip_camera_ios,
+                            color: Colors.white,
+                            size: 28,
+                          ),
                         ),
                       ),
                     ],
